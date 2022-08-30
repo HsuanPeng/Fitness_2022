5e6e10ae8e18b7fa7ea1a17270342b28828ff5fd
lisa <lisalisa12332180@gmail.com>
2022-08-30 13:54:42 +0800

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
-function App() {
+//components
+import PiePage from './PiePage';
+import LinePage from './LinePage';
+
+//firebase
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  collection,
+  addDoc,
+  getDocs,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  where,
+} from 'firebase/firestore';
+
+const PieOutside = styled.div`
+  padding-top: 100px;
+  width: 500px;
+  height: 500px;
+  margin: 0 auto;
+`;
+
+const LineOutside = styled.div`
+  padding-top: 100px;
+  width: 500px;
+  margin: 0 auto;
+`;
+
+const App = () => {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyA9Klg9ZWvbfrBEoraVnkpp38pzf7X3PCo',
+    authDomain: 'fitness-e8fc0.firebaseapp.com',
+    projectId: 'fitness-e8fc0',
+    storageBucket: 'fitness-e8fc0.appspot.com',
+    messagingSenderId: '678915191894',
+    appId: '1:678915191894:web:4db4938ac6d42615a777f7',
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  // const firestore = getFirestore(app);
+  const db = getFirestore(app);
+  //即時抓到資料夾名稱
+  const post = doc(collection(db, 'posts'));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PieOutside>
+        <PiePage />
+      </PieOutside>
+      <LineOutside>
+        <LinePage />
+      </LineOutside>
     </div>
   );
-}
+};
 
 export default App;
